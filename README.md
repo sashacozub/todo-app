272f7181aa6fa5d48589b6022edc34fae93c8a29
sashacozub <81567588+sashacozub@users.noreply.github.com>
2021-08-01 19:42:45 +0200

    Finish the first version of the app

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # Vanilla JavaScript ToDo App #
+![Project preview](https://i.imgur.com/ggxKqYE.png)
 
+---
 ## Project goals ##
 The goal of this ToDo App is to practice working with JavaScript code for tasks that might use similar logic.
+It was a first change to play around with local storage that would reload the app in its last state even when widow is reloaded or reopened.
 
+---
 ## Technologies ##
 This project is made with:
 * Vanilla JavaScript
 * SASS
 * HTML
-
-*Build in process...*
--- app.js
+++ app.js
@@ -1,49 +1,62 @@
 // Get DOM elements
 const form = document.querySelector('.todo');
 const input = document.querySelector('input');
-const taskList = document.querySelector('ul');
+const tasksList = document.querySelector('ul');
 const submitButton = document.querySelector('.submit');
 
 // Add task to the list on submit
 form.addEventListener('submit', e => {
     e.preventDefault();
-    if (input.value === '') {
+    if (input.value.trim() === '') {
         return;
     }
-
     createTask(input.value);
 })
 
+// Add task to the list when submit button is clicked
+submitButton.addEventListener('click', () => {
+    if (input.value.trim() === '') {
+        return;
+    }
+    createTask(input.value);
+});
 
 
+// Create a new list item with user's input as text
 const createTask = (task) => {
     const newTask = document.createElement('li');
-    task.charAt(0).toUpperCase();
     newTask.innerHTML = `
         <p>${task}</p>
         <i class="fas fa-check-square button check"></i>
         <i class="fas fa-trash-alt button remove"></i>
     `;
-
-    taskList.appendChild(newTask);
+    tasksList.appendChild(newTask);
+    saveToLocalStorage(tasksList.innerHTML);
     form.reset();
 }
 
 
-taskList.addEventListener('click', (e) => {
+// Check item as done or remove depending on which button was clicked
+tasksList.addEventListener('click', (e) => {
     if (e.target.classList.contains('remove')) {
-        e.target.parentNode.remove();
+        e.target.parentNode.classList.add('removed');
+        e.target.parentNode.addEventListener('transitionend', () => {
+            e.target.parentNode.remove();
+            saveToLocalStorage(tasksList.innerHTML);
+        })
     }
-
     if (e.target.classList.contains('check')) {
-        console.log('done')
+        e.target.parentNode.classList.toggle('checked');
+        saveToLocalStorage(tasksList.innerHTML);
     }
 })
 
-submitButton.addEventListener('click', () => {
-    if (input.value === '') {
-        return;
-    }
-    
-    createTask(input.value);
-});
+const saveToLocalStorage = list => {
+    localStorage.setItem('saved-todo', list);
+}
+
+window.addEventListener('load', () => {
+    tasksList.innerHTML = localStorage.getItem('saved-todo');
+})
+
+
